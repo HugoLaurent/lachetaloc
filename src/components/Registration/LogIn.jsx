95155edef4d9d9f319386cad0e3f6183671509df
HugoLaurent <lauhugopro@gmail.com>
2023-12-02 23:39:01 +0100

    Update sign in and log in buttons

--- src/components/Registration/LogIn.jsx
+++ src/components/Registration/LogIn.jsx
@@ -58,7 +58,7 @@ export default function LogIn({ switchRegistration, setSwitchRegistration }) {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Connectez vous!
               </button>
             </div>
           </form>
--- src/components/Registration/SignIn.jsx
+++ src/components/Registration/SignIn.jsx
@@ -11,6 +11,25 @@ export default function SignIn({ switchRegistration, setSwitchRegistration }) {
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label
+                htmlFor="pseudo"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Pseudo
+              </label>
+              <div className="mt-2">
+                <input
+                  id="pseudo"
+                  name="pseudo"
+                  type="pseudo"
+                  autoComplete="pseudo"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
                 htmlFor="email"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
@@ -39,6 +58,7 @@ export default function SignIn({ switchRegistration, setSwitchRegistration }) {
               </div>
               <div className="mt-2">
                 <input
+                  placeholder="12 caractères minimum"
                   id="password"
                   name="password"
                   type="password"
@@ -54,7 +74,7 @@ export default function SignIn({ switchRegistration, setSwitchRegistration }) {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Inscrivez vous !
               </button>
             </div>
           </form>
